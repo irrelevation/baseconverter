82b91490423e450b1b148922071100e3e347068e
Sebastian Vollnhals <sebastian@vollnhals.info>
2014-05-03 02:13:25 +0200

    Update baseconverter.js

--- baseconverter.js
+++ baseconverter.js
@@ -1,28 +1,32 @@
+#!/usr/bin/env node
 
+var bignum = require("bignum");
 
 module.exports = function (input, valid_in, valid_out){
 
-	var base_in = valid_in.length,
-		base_out = valid_out.length;
+	var base_in = bignum(valid_in.length);
+	var base_out = bignum(valid_out.length);
 
 	// convert input to base10
 	var l = input.length;
-	var base10 = 0;
+	var base10 = bignum(0);
 	var character;
 
 	for (var i = 0; i < input.length; i++) {
-		character = input[l-i-1];
-		base10 += valid_in.indexOf(character) * Math.pow(base_in, i);
+		character = input[(l-i)-1];
+		base10 = base10.add((base_in.pow(i)).mul((valid_in.indexOf(character)+1)));
 	}
 	
 	// convert base10 to base_out with valid_out charset
 	var result = '';
-	var remaining = base10;
+	var remaining = bignum(base10);
+
+	console.log(base10.toString());
 
 	do  {
-		result += valid_out.charAt(remaining % base_out);
-		remaining = Math.floor(remaining / base_out);
-	} while (remaining > 0);
+		result += valid_out.charAt(remaining.mod(base_out)-1);
+		remaining = (remaining.div(base_out));
+	} while (remaining.gt(0));
 
 	// reverse result
 	var reverse = '';
